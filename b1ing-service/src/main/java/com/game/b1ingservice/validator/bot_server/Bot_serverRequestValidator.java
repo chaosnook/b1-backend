cf316c2ddc0e4452dea2ce0e4fb5da14635b9320
zong <ingenium_@hotmail.com>
2021-01-21 00:05:10 +0700

    Update Bot ip validate

--- b1ing-service/src/main/java/com/game/b1ingservice/validator/bot_server/Bot_serverRequestValidator.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/validator/bot_server/Bot_serverRequestValidator.java
@@ -1,4 +1,21 @@
 package com.game.b1ingservice.validator.bot_server;
 
-public class Bot_serverRequestValidator {
+import com.game.b1ingservice.commons.Constants;
+import com.game.b1ingservice.exception.ErrorMessageException;
+import com.game.b1ingservice.payload.bot_server.Bot_serverRequest;
+import com.game.b1ingservice.postgres.repository.Bot_serverRepository;
+import com.game.b1ingservice.validator.CommonValidator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Bot_serverRequestValidator extends CommonValidator {
+    @Autowired
+    private Bot_serverRepository botServerRepository;
+
+    public void validate(Bot_serverRequest req){
+        if(StringUtils.isEmpty(req.getBotIp()))
+            throw new ErrorMessageException(Constants.ERROR.ERR_02009);
+        else if (!isIpAddress(req.getBotIp()))
+            throw new ErrorMessageException(Constants.ERROR.ERR_02013);
+        }
 }
