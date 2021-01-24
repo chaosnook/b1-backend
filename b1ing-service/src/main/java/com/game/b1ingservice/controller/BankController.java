bd7b20cd3843880a41cae403457de202330982b2
zong <ingenium_@hotmail.com>
2021-01-24 21:53:24 +0700

    Update path bank

--- b1ing-service/src/main/java/com/game/b1ingservice/controller/BankController.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/controller/BankController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/admin")
 @Slf4j
 public class BankController {
 
