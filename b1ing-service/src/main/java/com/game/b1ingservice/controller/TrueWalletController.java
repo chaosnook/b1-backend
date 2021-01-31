10fe5d419daa5a99bf881564553afffe9081f5ef
Tipakorn_ <tipakorn@orcsoft.co.th>
2021-01-31 14:04:32 +0700

    update: path test to admin

--- b1ing-service/src/main/java/com/game/b1ingservice/controller/TrueWalletController.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/controller/TrueWalletController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("api/test")
+@RequestMapping("api/admin")
 public class TrueWalletController {
 
     @Autowired
