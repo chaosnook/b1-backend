ff37ca2167c1c08a2b50d2ab53e741b035d5b5c6
zong <ingenium_@hotmail.com>
2021-02-16 00:39:52 +0700

    Update condition

--- b1ing-service/src/main/java/com/game/b1ingservice/payload/condition/ConditionResponse.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/payload/condition/ConditionResponse.java
@@ -3,8 +3,10 @@ package com.game.b1ingservice.payload.condition;
 import lombok.Data;
 
 @Data
-public class ConditionRequest {
+public class ConditionResponse {
+    private Long id;
     private int minTopup;
     private int maxTopup;
     private int bonus;
+    private Long agentId;
 }
---
+++ b1ing-service/src/main/java/com/game/b1ingservice/postgres/repository/ConditionRepository.java
@@ -0,0 +1,14 @@
+package com.game.b1ingservice.postgres.repository;
+
+import com.game.b1ingservice.postgres.entity.Condition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConditionRepository extends JpaRepository<Condition, Long>, JpaSpecificationExecutor<Condition> {
+
+    Optional<Condition> findById(Long id);
+}
