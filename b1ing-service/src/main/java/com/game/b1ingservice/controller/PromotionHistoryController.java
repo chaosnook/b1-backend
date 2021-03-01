b2aed56a97468480b5422fc77481b4aaa2449e94
zong <ingenium_@hotmail.com>
2021-03-01 23:57:24 +0700

    update promotion his

---
+++ b1ing-service/src/main/java/com/game/b1ingservice/controller/PromotionHistoryController.java
@@ -0,0 +1,13 @@
+package com.game.b1ingservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/admin")
+@Slf4j
+public class PromotionHistoryController {
+
+    @
+}
