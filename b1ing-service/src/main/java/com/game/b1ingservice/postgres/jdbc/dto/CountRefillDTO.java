bd495a7355b09f5cb75448e7c94b9cfdc7c348aa
Natthawut Jantapoon <dear.natthawut@gmail.com>
2021-04-24 15:40:20 +0700

    update query profit

--- b1ing-service/src/main/java/com/game/b1ingservice/postgres/jdbc/dto/CountRefillDTO.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/postgres/jdbc/dto/CountRefillDTO.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 
 @Data
 public class CountRefillDTO {
+    private Long id;
     private String username;
     private Integer depositCount;
     private BigDecimal Deposit;
