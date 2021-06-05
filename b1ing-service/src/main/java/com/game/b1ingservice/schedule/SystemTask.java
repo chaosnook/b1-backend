8f7fdedd7371ddb985a6e4ce4296164d8cbee291
Natthawut Jantapoon <dear.natthawut@gmail.com>
2021-06-05 17:00:41 +0700

    Update SystemTask.java

--- b1ing-service/src/main/java/com/game/b1ingservice/schedule/SystemTask.java
+++ b1ing-service/src/main/java/com/game/b1ingservice/schedule/SystemTask.java
@@ -2,6 +2,8 @@ package com.game.b1ingservice.schedule;
 
 import com.game.b1ingservice.service.MistakeService;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SystemTask {
 
+    @Autowired
     private MistakeService mistakeService;
 
     @Scheduled(cron = "${b1ing.schedule.mistake}")
+    @SchedulerLock(name = "scheduleClearMistakeTask",
+            lockAtLeastForString = "PT10M", lockAtMostForString = "PT10M")
     public void scheduleClearMistakeTask() {
         try {
             mistakeService.clearLimit();
